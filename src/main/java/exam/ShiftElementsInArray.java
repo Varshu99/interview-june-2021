f91302f5b89eb37ac076cc4214b7242372b36d88
Varshu99 <72430323+Varshu99@users.noreply.github.com>
2021-06-22 13:02:07 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -1,18 +1,23 @@
 package exam;
 
-public class ShiftElementsInArray {
+public class CountCombinations {
 
-  public void shift(int[] arr) {
+  public int getNumberOfWays(final int total, final int k) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-String x = "";
-        while(n > 0)
-        {
-            int a = n % 2;
-            x = a + x;
-            n = n / 2;
-        }
-    return x;
-
+    int n=1;
+		for(int i = 0; i < n; i++)
+		{    
+            int j, last;    
+            last = arr[arr.length-1];   
+            for(j = arr.length-1; j > 0; j--)
+            {    
+                arr[j] = arr[j-1];    
+            }    
+            arr[0] = last;    
+     } 
+        for(int i=0;i<arr.length;i++)
+        System.out.print(arr[i]+" ")
+    return 0;
   }
 
 }
